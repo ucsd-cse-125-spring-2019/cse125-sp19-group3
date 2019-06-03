20413dd6aa6f833fb243bc20a73b138a01817741
lingfeng <lig012@ucsd.edu>
2019-06-02 22:59:18 -0700

    ui prepare phase switch fix

--- rendering/GUILayout.h
+++ rendering/GUILayout.h
@@ -375,7 +375,7 @@ static void ui_round_results(struct nk_context *ctx, struct media *media,
 		*it = -1;		// reset current max to -1
 	}
 	if (nk_begin(ctx, "round result", nk_rect(0, 0, width, height),
-		NK_WINDOW_NO_SCROLLBAR))
+		NK_WINDOW_NO_SCROLLBAR | NK_WINDOW_BACKGROUND))
 	{
 		ui_prepare_title(ctx, media, width, height, "Summary", game);
 		nk_style_set_font(ctx, &(media->font_64->handle));
@@ -458,9 +458,8 @@ static void ui_round_results(struct nk_context *ctx, struct media *media,
 
 	}
 	nk_end(ctx);
-
 	if (nk_begin(ctx, "SwitchPage", nk_rect(0.03*width, 0.9*height, height*0.1, height*0.1),
-		NK_WINDOW_NO_SCROLLBAR)) {
+		NK_WINDOW_NO_SCROLLBAR| NK_WINDOW_BORDER)) {
 		nk_layout_row_static(ctx, height*0.1, height*0.1, 1);
 		if (nk_button_symbol(ctx, NK_SYMBOL_TRIANGLE_RIGHT)) {
 			currPreparePage = 1;
@@ -472,7 +471,7 @@ static void ui_round_results(struct nk_context *ctx, struct media *media,
 static void ui_shop(struct nk_context *ctx, struct media *media, int width, int height, ScenePlayer * player, int & currPreparePage, ClientGame * game) {
 
 	if (nk_begin(ctx, "Prepare", nk_rect(0, 0, width, height),
-		NK_WINDOW_NO_SCROLLBAR
+		NK_WINDOW_NO_SCROLLBAR|NK_WINDOW_BACKGROUND
 	))
 	{
 		char* skill_string[4] = { "Evade", "Projectile", "AOE", "Cone AOE" };
@@ -541,7 +540,7 @@ static void ui_shop(struct nk_context *ctx, struct media *media, int width, int 
 	nk_end(ctx);
 
 	if (nk_begin(ctx, "SwitchPageBack", nk_rect(0.03*width, 0.9*height, height*0.1, height*0.1),
-		NK_WINDOW_NO_SCROLLBAR)) {
+		NK_WINDOW_NO_SCROLLBAR| NK_WINDOW_BORDER)) {
 		nk_layout_row_static(ctx, height*0.1, height*0.1, 1);
 		if (nk_button_symbol(ctx, NK_SYMBOL_TRIANGLE_LEFT)) {
 			currPreparePage = 0;
