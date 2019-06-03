71859b268ee83f0ae4bc6da43a494ca93410e037
Yi H Chen <yhc001@UCSD.EDU>
2019-06-03 16:59:05 -0700

    started game reset logic

--- networking/KillStreak/ServerGame.cpp
+++ networking/KillStreak/ServerGame.cpp
@@ -588,7 +588,23 @@ void ServerGame::resetValuesPreKillPhase()
 	std::fill(leaderBoard->currentDeaths.begin(), leaderBoard->currentDeaths.end(), 0);
 
 	// TODO: What other values do we need to reset/update?
+	//position
+	//move this to ServerScene
+	for (std::pair<unsigned int, ScenePlayer> player : scene->scenePlayers) {
+		player.second.playerRoot->translation = glm::translate(glm::mat4(1.0f), spawn_loc[player.first]);
+		player.second.currentPos = spawn_loc[player.first];
+		player.second.setDestination(spawn_loc[player.first]);
+	}
 
+	//cooldown
+
+	//death / alive ?
+
+	//invis
+
+	//silence
+
+	//charge
 }
 
 
