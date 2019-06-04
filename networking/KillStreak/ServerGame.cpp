8326515274c85ebb89888b69b8854ab79e71c229
Aldo Sevada <aldosevada@gmail.com>
2019-06-03 21:25:04 -0700

    gold bug fixed

--- networking/KillStreak/ServerGame.cpp
+++ networking/KillStreak/ServerGame.cpp
@@ -477,16 +477,18 @@ bool ServerGame::updateKillPhase() {
 		memcpy(&next_packet, &serverTickPacket, sizeof(serverTickPacket));
 
 		char * temp_data = next_packet.data;
+
 		// set first byte of data to players dead/alive state
 		memcpy(temp_data, &p_it->second->alive, sizeof(bool));
-
-		// set first byte of data to silenced
 		temp_data += sizeof(bool);
-		memcpy(temp_data, &p_it->second->silenced, sizeof(bool));
 
 		// set client gold after first byte
-		temp_data += sizeof(bool);
 		memcpy(temp_data, &p_it->second->gold, sizeof(int));
+		temp_data += sizeof(int);
+
+		// set first byte of data to silenced
+		memcpy(temp_data, &p_it->second->silenced, sizeof(bool));
+		temp_data += sizeof(bool);
 
 		network->sendToClient(client_id, next_packet);
 		p_it++;
@@ -509,7 +511,7 @@ bool ServerGame::updatePreparePhase() {
 		inputPackets.push_back(vector<ClientInputPacket*>());
 	}
 
-	// Drain all packets from all client inputs
+	// Drain all packets from all client boolinputs
 	for (auto client_data : client_data_list) {
 		int i = client_data->id;
 		ClientThreadQueue * q_ptr = client_data->q_ptr;
@@ -723,6 +725,13 @@ ServerInputPacket ServerGame::createServerTickPacket() {
 	memcpy(bufPtr, &client_gold, sizeof(int));
 	bufPtr += sizeof(int);
 	sgSize += sizeof(int);
+
+	// TODO: REMOVE ME!
+	if (client_gold != 0)
+	{
+		logger()->debug("Create tick packet serialized gold {}", client_gold);
+	}
+
 
 	// serialize if user silenced; default init the first byte (silenced)
 	bool silenced = false;
